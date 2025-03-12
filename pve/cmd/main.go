00629f4a60d023c873f02dfad17f5b312c3bf6d8
田家杰 <tjj2937693919@outlook.com>
2025-03-12 19:31:27 +0800

    3_12

--- pve/cmd/main.go
+++ pve/cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
 func main() {
-
+	
+	//router := gin.Default()
+	//
+	//vmRouter := router.Group("/vm")
+	{
+		//// 创建虚拟机(实际是使用复制模板的方式创建)
+		//vmRouter.POST("/create", createVM)
+		
+		//// 删除虚拟机
+		//vmRouter.POST("/delete", deleteVM)
+		
+		//// 启动虚拟机
+		//vmRouter.POST("/start", startVM)
+		
+		//// 关闭虚拟机
+		//vmRouter.POST("/stop", stopVM)
+		
+		//// 重启虚拟机
+		//vmRouter.POST("/restart", restartVM)
+		
+		//// 获取虚拟机列表
+		//vmRouter.GET("/list", listVM)
+		
+		//// 获取虚拟机详情
+		//vmRouter.GET("/detail", detailVM)
+		
+		//// 获取虚拟机ip
+		//vmRouter.GET("/ip", getVMIP)
+	}
 }
